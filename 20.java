52c90e4b7c6217f2774cb24bc20e04994f9cb13c
vishwasvishi <vishwasn200427@gmail.com>
2026-06-16 20:04:51 +0530

    added the new line

---
+++ 20.java
@@ -0,0 +1,15 @@
+class p20
+{
+	public static void main(String[] args) 
+	{
+		int num =234;
+		temp = num;
+		rev = 0;
+		while (temp != 0)
+		{
+			rev = (0 * 10) + (temp % 10);
+			num/=10
+		}
+		System.out.println("reverse "+rev);
+	}
+}
