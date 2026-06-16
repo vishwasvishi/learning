f60385efa6c50c589f805ec2b1591949880fda2f
vishwasvishi <vishwasn200427@gmail.com>
2026-06-16 21:46:58 +0530

    java file

--- 20.java
+++ 20.java
@@ -3,12 +3,12 @@ class p20
 	public static void main(String[] args) 
 	{
 		int num =234;
-		temp = num;
-		rev = 0;
+		int temp = num;
+		int rev = 0;
 		while (temp != 0)
 		{
-			rev = (0 * 10) + (temp % 10);
-			num/=10
+			rev = (rev * 10) + (temp % 10);
+			temp/=10;
 		}
 		System.out.println("reverse "+rev);
 	}
